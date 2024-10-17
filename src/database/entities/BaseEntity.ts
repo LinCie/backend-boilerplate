f9ce155a84933ca5cf0557ad333cd1b366c4589c
LinCie <aldiofernanda@protonmail.com>
2024-10-17 22:40:55 +0700

    added basic base entity

---
+++ src/database/entities/BaseEntity.ts
@@ -0,0 +1,22 @@
+import {
+  Entity,
+  PrimaryKey,
+  Property,
+  SerializedPrimaryKey,
+} from '@mikro-orm/core';
+import { ObjectId } from '@mikro-orm/mongodb';
+
+@Entity()
+export class BaseEntity {
+  @PrimaryKey()
+  _id!: ObjectId;
+
+  @SerializedPrimaryKey()
+  id!: string;
+
+  @Property()
+  createdAt: Date = new Date();
+
+  @Property({ onUpdate: () => new Date() })
+  lastUpdated: Date = new Date();
+}
