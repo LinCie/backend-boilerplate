426d9cfc3b26b25e1d268b144ecd6b45bee1057e
LinCie <aldiofernanda@protonmail.com>
2024-10-18 11:52:01 +0700

    remove password from update user dto

--- src/dtos/users/update-user.dto.ts
+++ src/dtos/users/update-user.dto.ts
@@ -8,13 +8,4 @@ export class UpdateUserDto {
     message: "Username should not contain spaces",
   })
   readonly username?: string;
-
-  @IsOptional()
-  @IsString()
-  @MinLength(8)
-  @Matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}/, {
-    message:
-      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
-  })
-  readonly password?: string;
 }
